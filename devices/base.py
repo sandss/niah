b4d3571087870a48329110e20108d5506f50b3a5
Skylar Sands <scshhs@gmail.com>
2017-08-20 23:58:31 -0500

    clean up base

--- devices/base.py
+++ devices/base.py
@@ -1,16 +1,10 @@
 import pexpect
 from niah import errors
-class BaseIOS(object):
-
-    def __init__ (self,log):
+class BaseDevice(object):
+    #TODO: Add logging
+    def __init__ (self,session):
         self.hostname = none
-        self.logger = log
-
-    def _get_hostname(self):
-        self.child.send("\n")
-        self.child.expect(['#', '>'], 2)
-        text = self.child.before
-        self.hostname = text.replace('>', '').replace('#', '').strip()
+        self.child = session
 
     def expect(self,pattern):
         return self.child.expect(pattern)
@@ -18,8 +12,19 @@ class BaseIOS(object):
     def write(self,command):
         self.child.sendline(command)
 
+class BaseIOS(BaseDevice):
+    #TODO: Add logging
+    def __init__ (self,session):
+        super(BaseDevice, self).__init__(session)
+
+    def _get_hostname(self):
+        self.child.send("\n")
+        self.child.expect(['#', '>'], 2)
+        text = self.child.before
+        self.hostname = text.replace('>', '').replace('#', '').strip()
+
     def cmd(self,command):
-        self.logger.info("Sending Command: %s"%(command))
+        #self.logger.info("Sending Command: %s"%(command))
         self.logger.debug(self.child.before)
         self.write(command)
 
@@ -29,33 +34,26 @@ class BaseIOS(object):
             self.child.expect('#')
 
         ret_text = self.child.before
-        self.logger.debug(ret_text)
+        #self.logger.debug(ret_text)
         if 'hostname' in ret_text:
             self._get_hostname()
 
         if "Invalid input" in ret_text or "Incomplete command" in ret_text:
-            self.logger.info("Invalid Command: " + command)
+            #self.logger.info("Invalid Command: " + command)
             raise errors.InvalidCommand(command, self.ip)
 
         return ''.join(ret_text.splitlines(True)[1:])
 
-class BaseWLC(object):
+class BaseWLC(BaseDevice):
 
     def __init__ (self,session):
-        self.hostname = none
-        self.child = session
+        super(BaseDevice, self).__init__(session)
 
     def _get_hostname(self):
         self.child.send("\n")
         self.child.expect('>')
         text = self.child.before
         self.hostname = text.replace(' ) >', '').replace('( ', '').strip()
-
-    def expect(self,pattern):
-        return self.child.expect(pattern)
-
-    def write(self,command):
-        self.child.sendline(command)
 
     def cmd(self,command):
         #self.logger.info("Sending Command: %s"%(command))
